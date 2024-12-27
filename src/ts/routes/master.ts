6a6ba64e0fc8efc0ecc48fd369643886eef906b5
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-12-27 01:16:07 -0600

    Default page loads README, will be updated later

--- src/ts/routes/master.ts
+++ src/ts/routes/master.ts
@@ -1,8 +1,31 @@
 import { Router } from "express";
+import { readFile } from "fs/promises";
+import DOMPurify from "isomorphic-dompurify";
+import { marked } from "marked";
+import { join } from "path";
 export const router = Router();
 
 router.use((_req, res) => {
-  res.render("default");
+  readFile(join(process.cwd(), "./README.md"), "utf-8")
+    .then((md) => {
+      marked
+        .parse(md, { async: true })
+        .then((dom) => {
+          res.render("md", {
+            markdown: DOMPurify.sanitize(dom),
+          });
+        })
+        .catch((error) => {
+          res.render("md", {
+            markdown: error.message,
+          });
+        });
+    })
+    .catch((error: Error) => {
+      res.render("md", {
+        markdown: error.message,
+      });
+    });
 });
 
 export default router;
