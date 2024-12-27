61352127f06f8a3c1f36fa10a421db6daf00385e
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-12-27 02:24:25 -0600

    Move main page to home.ts

---
+++ src/ts/routes/web/home.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { readFile } from "fs/promises";
+import DOMPurify from "isomorphic-dompurify";
+import { marked } from "marked";
+import { join } from "path";
+export const router = Router();
+
+router.get("/", (_req, res) => {
+  readFile(join(process.cwd(), "./README.md"), "utf-8")
+    .then((md) => {
+      marked
+        .parse(md, { async: true })
+        .then((dom) => {
+          res.render("md", {
+            title: "Modpacker Distro Server",
+            markdown: DOMPurify.sanitize(dom),
+          });
+        })
+        .catch((error) => {
+          res.render("md", {
+            title: "Error",
+            markdown: error.message,
+          });
+        });
+    })
+    .catch((error: Error) => {
+      res.render("md", {
+        title: "Error",
+        markdown: error.message,
+      });
+    });
+});
+
+export default router;
