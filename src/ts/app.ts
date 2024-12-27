715e5e443fb462396509d8e4911c3cd09cacc410
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-12-27 00:03:45 -0600

    We dont need pkg

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,7 +1,38 @@
+// Modules
 import express from "express";
 import { engine } from "express-handlebars";
 import http from "http";
 import { join } from "path";
+import Database from "@gavinhsmith/simpledatabase";
+
+// Initiate Database
+const db = new Database(join(process.cwd(), "database.db"));
+
+console.info("Loading database...");
+db.exists("users")
+  .then(() => {
+    console.info("Loaded table 'users'");
+  })
+  .catch(() => {
+    console.info("Creating table 'users'...");
+    db.create("users", [
+      {
+        name: "id",
+        type: "INTENGER",
+        isPrimaryKey: true,
+      },
+      {
+        name: "username",
+        type: "TEXT",
+      },
+      {
+        name: "password",
+        type: "TEXT",
+      },
+    ]).then(() => {
+      console.info("Created table 'users'");
+    });
+  });
 
 // Initiate server
 console.info("Starting server...");
@@ -15,13 +46,13 @@ app.engine(
   engine({
     extname: ".hbs",
     defaultLayout: "main",
-    layoutsDir: join(__dirname, "../web/views/layouts"),
-    partialsDir: join(__dirname, "../web/views/partials"),
+    layoutsDir: join(process.cwd(), "./web/views/layouts"),
+    partialsDir: join(process.cwd(), "./web/views/partials"),
   })
 );
 app.set("view engine", "hbs");
-app.set("views", join(__dirname, "../web/views"));
-app.use(express.static(join(__dirname, "../web/public")));
+app.set("views", join(process.cwd(), "./web/views"));
+app.use(express.static(join(process.cwd(), "./web/public")));
 
 app.use((_req, res) => {
   res.render("default");
