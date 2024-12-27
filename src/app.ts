62ba78afb4d3cd2143b40787874a636ec4716f51
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-12-26 18:08:30 -0600

    Initiate express server

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import http from "http";
+
+// Initiate server
+console.info("Starting server...");
+
+const app = express();
+const server = http.createServer(app);
+
+// Start server
+server.listen(8080, () => {
+  console.info("Server started at *:8080");
+});
