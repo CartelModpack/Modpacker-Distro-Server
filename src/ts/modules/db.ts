7a36a4a7a4b150813b51db877b5022b1b9003b44
Gavin Smith <39228744+gavinhsmith@users.noreply.github.com>
2024-12-28 02:01:18 -0600

    Added default admin account, pass is admin

--- src/ts/modules/db.ts
+++ src/ts/modules/db.ts
@@ -2,6 +2,7 @@ import Database from "@gavinhsmith/simpledatabase";
 import { TableColumnSettings } from "@gavinhsmith/simpledatabase/types/DatabaseTypes.js";
 import { join } from "path";
 import dbConfig from "../../../config/db.config.json" with {type: "json"};
+import { AuthUserAccount } from "../routes/middleware/auth.js";
 
 let tables = <string[]>dbConfig.tables;
 let configs = <TableColumnSettings[][]>dbConfig.configs;
@@ -40,20 +41,49 @@ export function loadDatabase(): Promise<void> {
       .then(() => {
         Promise.all(promises).then(() => {
           db.table<{key: string, value: string}>("mds_data").allEntries().then((data) => {
+            let createVersKey = true;
             for (let d of data) {
               if (d.key === "db_version") {
-                if (d.value === String(version)) {
-                  resolve();
-                } else {
+                createVersKey = false;
+                if (d.value !== String(version)) {
                   // Will go into updater later, but for now this is fine. 
                   reject(new Error(`DB config versions are not equal. Expected ${version}, was ${d.value}`));
                 }
-                return;
               }
             }
-            db.table("mds_data").add({key: "db_version", value: String(version)}).then(() => {
-              resolve();
-            }).catch(reject);
+            if (createVersKey) {
+              db.table("mds_data").add({key: "db_version", value: String(version)}).then(() => {
+                db.table<AuthUserAccount>("user_accounts").allEntries().then((accs) => {
+                  if (accs.length <= 0) {
+                    db.table<AuthUserAccount & {id: 0}>("user_accounts").add({
+                      username: "admin",
+                      // sha512 for "admin" 
+                      hash: "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec",
+                      id: 0
+                    }).then(() => {
+                      resolve();
+                    }).catch(reject);
+                  } else {
+                    resolve()
+                  }
+                }).catch(reject);
+              }).catch(reject);
+            } else {
+              db.table<AuthUserAccount>("user_accounts").allEntries().then((accs) => {
+                if (accs.length <= 0) {
+                  db.table<AuthUserAccount & {id: 0}>("user_accounts").add({
+                    username: "admin",
+                    // sha512 for "admin" 
+                    hash: "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec",
+                    id: 0
+                  }).then(() => {
+                    resolve();
+                  }).catch(reject);
+                } else {
+                  resolve()
+                }
+              }).catch(reject);
+            }
           }).catch(reject);
         }).catch(reject);
       })
--- src/ts/routes/middleware/auth.ts
+++ src/ts/routes/middleware/auth.ts
@@ -9,10 +9,13 @@ import { createHash } from "crypto";
 declare global {
   namespace Express {
     interface Request {
-      /** If this user is logged in. */
-      loggedIn: boolean;
-      /** Session information for the user. Is not defined unless {@link Request.loggedIn} is true. */
-      session?: AuthTokenData;
+      /** Auth data for the user. */
+      auth: {
+        /** If this user is logged in. */
+        loggedIn: boolean;
+        /** Session information for the user. Is not defined unless {@link Request.loggedIn} is true. */
+        session?: AuthTokenData;
+      };
     }
   }
 }
@@ -45,10 +48,12 @@ export type AuthUserHashType = "sha1" | "sha256" | "sha512";
 function checkExpiration(expires: Date): boolean {
   return new Date().getTime() < expires.getTime();
 }
+
 /** Gets a date that is `ms` milliseconds before/after the current date. */
 function getRelativeDate(ms: number): Date {
   return new Date(new Date().getTime() + ms);
 }
+
 /** Turn the formidable `fields` property into something easier to use. */
 function makeAuthFormDataReadable(fields: Fields<string>): AuthUserFormData {
   let remember_me: boolean =
@@ -59,10 +64,12 @@ function makeAuthFormDataReadable(fields: Fields<string>): AuthUserFormData {
     remember_me,
   };
 }
+
 /** Calculate a hash */
 function calculateHash(password: string, type: AuthUserHashType): string {
   return createHash(type).update(password, "utf8").digest("hex");
 }
+
 /** Checks if an account is valid. */
 function verifyAccount(
   incoming: AuthUserFormData,
@@ -71,6 +78,7 @@ function verifyAccount(
   let hash = calculateHash(incoming.password, "sha512");
   return incoming.username === existing.username && hash === existing.hash;
 }
+
 /** Generates a session cookie */
 function generateSession(username: string): AuthTokenData {
   return {
@@ -88,8 +96,10 @@ export function processAuthToken(
   res: Response,
   next: WebErrorNextFunction
 ) {
-  req.loggedIn = false;
-  req.session = null;
+  req.auth = {
+    loggedIn: false,
+    session: null,
+  };
 
   if (req.cookies["Auth-Token"]) {
     let cookie: AuthTokenData = JSON.parse(req.cookies["Auth-Token"]);
@@ -98,31 +108,36 @@ export function processAuthToken(
     db.table<AuthTokenData>("user_auth_tokens")
       .allEntries()
       .then((tokens) => {
+        let found = false;
         for (let token of tokens) {
           if (
             // Checks if a cookie matches a token and it is not expired, then sets the session.
             token.token === cookie.token &&
             checkExpiration(new Date(token.expires))
           ) {
-            req.loggedIn = true;
-            req.session = cookie;
+            found = true;
+            req.auth.loggedIn = true;
+            req.auth.session = token;
             next(); // Go on to next handler.
           } else if (
             // Checks if a cookie matches a token and it is expired, then clears the cookie and token.
             token.token === cookie.token &&
             !checkExpiration(new Date(token.expires))
           ) {
+            found = true;
             res.clearCookie("Auth-Token"); // Clear user cookie.
             db.table<AuthTokenData>("user_auth_tokens") // Clear token from db.
               .delete("token", token.token)
               .then(() => {
                 next();
               })
-              .catch(sendPromiseCatchError(500, req, res, next));
+              .catch(() => {
+                next();
+              });
           }
         }
 
-        next(); // Go on to next handler, no matching token found.
+        if (!found) next(); // Go on to next handler, no matching token found.
       })
       .catch(sendPromiseCatchError(500, req, res, next)); // This will go to the error handler.
   } else {
@@ -155,8 +170,10 @@ export function processLoginAttempt(
             // Verify acc
             if (verifyAccount(incoming, acc)) {
               found = true;
+
               // Create session.
               let session = generateSession(acc.username);
+
               db.table<AuthTokenData>("user_auth_tokens")
                 .add(session)
                 .then(() => {
